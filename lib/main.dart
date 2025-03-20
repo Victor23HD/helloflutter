1a2fdfb0a87b0595bfe1443da7250ca03790b466
Victor Oliveira <xitornet.23@gmail.com>
2025-03-20 18:29:50 -0300

    build: Ready-made business rule

--- lib/main.dart
+++ lib/main.dart
@@ -1,201 +1,49 @@
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
+  String email = "xitornet.23@icloud.com";
+  String password = "Senha123";
+  final isValid = validate(email: email, password: password);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(title: Text("Home")),
-        body: ListView(
-          children: [
-            Task(
-              "Aprender IA",
-              "https://cdn-icons-png.flaticon.com/512/2111/2111122.png",
-              3,
-            ),
-            Task(
-              "Aprender a programar",
-              "https://cdn-icons-png.flaticon.com/512/1995/1995515.png",
-              5,
-            ),
-            Task(
-              "Aprender a cozinhar",
-              "https://cdn-icons-png.flaticon.com/512/1830/1830839.png",
-              4,
-            ),
-            Task(
-              "Aprender a dirigir",
-              "https://cdn-icons-png.flaticon.com/512/4669/4669518.png",
-              3,
-            ),
-            Task(
-              "Aprender a desenhar",
-              "https://cdn-icons-png.flaticon.com/512/4335/4335667.png",
-              2,
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
-      ),
-    );
+  if (isValid == null) {
+    login(email: email, password: password);
+  } else {
+    print(isValid);
   }
 }
 
-class Task extends StatefulWidget {
-  final String name;
-  final String foto;
-  final int stars;
-  const Task(this.name, this.foto, this.stars, {Key? key}) : super(key: key);
+void login({required String email, required String password}) {
+  final response = validateLogin(email: email, password: password);
 
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              height: 140,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: Colors.blue,
-              ),
-            ),
-
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.black26,
-                          borderRadius: BorderRadius.circular(4),
-                        ),
-                        width: 72,
-                        height: 100,
-                        child: Image.network(widget.foto, fit: BoxFit.contain),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 200,
-                              child: Text(
-                                widget.name,
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              ),
-                            ),
-                            Widget_Stars(widget.stars),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Container(
-                          height: 46,
-                          width: 72,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                if (nivel != 10) {
-                                  nivel++;
-                                }
-                              });
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Icon(Icons.keyboard_arrow_up_outlined),
-                                Text("UP", style: TextStyle(fontSize: 12)),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Container(
-                        width: 200,
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: nivel / 10,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Container(
-                        child: Text(
-                          "Nivel: ${nivel}",
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+  if(response == true){
+    print("Abrindo a HomePage");
   }
 }
 
-class Widget_Stars extends StatefulWidget {
-  final int stars;
-  const Widget_Stars(this.stars, {Key? key}) : super(key: key);
+String? validate({required email, required String password}) {
+  final emailIsValid = validateEmail(email: email);
+  final passwordIsValid = validatePassword(password: password);
 
-  @override
-  State<Widget_Stars> createState() => _Widget_StarsState();
+  if (emailIsValid == false) {
+    return "the email is not valid";
+  } else if (passwordIsValid == false) {
+    return "the password is not valid";
+  }
+
+  return null;
 }
 
-class _Widget_StarsState extends State<Widget_Stars> {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: List.generate(5, (index) {
-        return Icon(
-          Icons.star,
-          size: 16,
-          color: index < widget.stars ? Colors.blue : Colors.blue[100],
-        );
-      }),
-    );
-  }
+bool validateEmail({required String email}) {
+  // ignore: unnecessary_null_comparison
+  return email != null && email.length > 0;
+}
+
+bool validatePassword({required String password}) {
+  // ignore: unnecessary_null_comparison
+  return password != null && password.length >= 6;
+}
+
+bool validateLogin({required String email, required String password}) {
+  print("Conectando....");
+  print("validando suas credenciais...");
+
+  return true;
 }
